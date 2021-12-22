0b7174180d7a4691bd87fb4b82b07014ea323e41
vishal-zx <19ucs053@lnmiit.ac.in>
2021-12-22 08:57:11 +0530

    patient history by doctor updated

--- LNMMedicalDispensary/users/views.py
+++ LNMMedicalDispensary/users/views.py
@@ -25,7 +25,8 @@ def register(request):
         if form.is_valid():
             user = form.save()
             msg = 'user created'
-            patient=Patient(Pid=request.user.id,name=user.username,age=user.age,gender=user.gender)
+            patient = Patient(
+                Pid=request.user.id, name=user.username, age=user.age, gender=user.gender)
             patient.save()
             return redirect('login_view')
         else:
@@ -43,7 +44,7 @@ def login_view(request):
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
             user = authenticate(username=username, password=password)
-            
+
             if user is not None and user.is_doctor:
                 login(request, user)
                 return redirect('doctor')
@@ -78,10 +79,8 @@ def scheduleTest(request):
     return render(request, 'scheduleTest.html')
 
 
-#def patientHistory(request):
+# def patientHistory(request):
  #   return render(request, 'PatientHistory.html')
-
-
 
 
 def viewPatientHistory(request):
@@ -101,20 +100,21 @@ def bookAppointment(request):
 
 
 def checkAppointment(request):
-    appointment=Appointment.objects.all()
-    Appointments=[{}]
-    sr=1
-    user=request.user
+    appointment = Appointment.objects.all()
+    Appointments = [{}]
+    sr = 1
+    user = request.user
     for i in appointment:
-        if i.Did.Did==user.id:
-            Appointments.insert(sr-1,{'sr':sr,'Timings':i.Timings,'name':Patient.objects.get(Pid=i.Pid.Pid).name,'mailid':i.mailid})
-            sr=sr+1
+        if i.Did.Did == user.id:
+            Appointments.insert(sr-1, {'sr': sr, 'Timings': i.Timings,
+                                       'name': Patient.objects.get(Pid=i.Pid.Pid).name, 'mailid': i.mailid})
+            sr = sr+1
     Appointments.pop()
-    print(len(Appointments))  
-    
-    #print(request.GET['cancel'])
-    #patient=Patient.objects.get()
-    return render(request, 'checkAppointment.html',{'Appointments': Appointments})
+    print(len(Appointments))
+
+    # print(request.GET['cancel'])
+    # patient=Patient.objects.get()
+    return render(request, 'checkAppointment.html', {'Appointments': Appointments})
 
 
 def Treatment(request):
@@ -125,28 +125,30 @@ def DoctorProfile(request):
     # doctor=Doctor.objects.all()
     # profile={}
     # sr=1
-    user=request.user
+    user = request.user
     # for i in doctor:
     #     if i.Did==user.id:
     #         profile[sr]={'sr':sr,'name':i.name,'did':i.Did,'age':i.age,'Gender':i.gender,'address':i.address}
     #         sr=sr+1
-    #profile.pop()
+    # profile.pop()
     print(user.id)
-    #profile=Doctor.objects.get(Did=user.id)
+    # profile=Doctor.objects.get(Did=user.id)
     return render(request, 'DoctorProfile.html')
 
 
 def ChemistProfile(request):
     user = request.user
-    form = ChemistForm(instance = user)
+    form = ChemistForm(instance=user)
     if request.method == 'POST':
-        form = ChemistForm(request.POST, request.FILES, instance=user) #The request.POST data will be send to the project instance
+        # The request.POST data will be send to the project instance
+        form = ChemistForm(request.POST, request.FILES, instance=user)
         if form.is_valid():
-            form.save() #IT will modify the project
-            #signal for profile updated to be put
-            return redirect('chemistProfile') #user will be redirected to the projects page
-    
-    context = {'form' : form, 'user' : user}
+            form.save()  # IT will modify the project
+            # signal for profile updated to be put
+            # user will be redirected to the projects page
+            return redirect('chemistProfile')
+
+    context = {'form': form, 'user': user}
     return render(request, 'chemistProfile.html', context)
 
 
@@ -157,11 +159,13 @@ def checkMedicine(request):
        # print(request.POST)  # So we see its a dictionary of attributes of project and its details
         form = MedicineForm(request.POST, request.FILES)
         # if form.is_valid():
-            #form.save()
-        return redirect('updateMedicine', request.POST['Mid']) #user will be redirected to the projects page
-    
-    context = {'form' : form, 'medicines' : medicine}
+        # form.save()
+        # user will be redirected to the projects page
+        return redirect('updateMedicine', request.POST['Mid'])
+
+    context = {'form': form, 'medicines': medicine}
     return render(request, 'checkMedicine.html', context)
+
 
 def updateMedicine(request, pk):
     current_medicine = Medicine.objects.get(Mid=pk)
@@ -169,11 +173,14 @@ def updateMedicine(request, pk):
     # print(medicine)
     form = MedicineInstance(instance=current_medicine)
     if request.method == 'POST':
-        form = MedicineInstance(request.POST, request.FILES, instance=current_medicine) #The request.POST data will be send to the project instance
+        # The request.POST data will be send to the project instance
+        form = MedicineInstance(
+            request.POST, request.FILES, instance=current_medicine)
         if form.is_valid():
-            form.save() #IT will modify the project
-            return redirect('checkMedicine') #user will be redirected to the projects page
-    context = {'form':form, 'medicines' : medicine}
+            form.save()  # IT will modify the project
+            # user will be redirected to the projects page
+            return redirect('checkMedicine')
+    context = {'form': form, 'medicines': medicine}
     return render(request, "updateMedicine.html", context)
 
 
@@ -181,7 +188,7 @@ def MedicineRecord(request):
     medicine = Medicine.objects.all()
     # for i in medicine:
     # print(medicine)
-    context = {'medicines' : medicine}
+    context = {'medicines': medicine}
 
     return render(request, 'medicineRecord.html', context)
 
@@ -196,13 +203,13 @@ def issueMedicine(request):
             med = request.POST['medicine']
         except MultiValueDictKeyError:
             med = False
-        medicine = Medicine.objects.get(Mid = med)
+        medicine = Medicine.objects.get(Mid=med)
         Qy = int(medicine.Quantity)
         try:
             qy = int(request.POST['quantity'])
         except MultiValueDictKeyError:
             qy = False
-        
+
         if Qy >= qy:
             medicine.Quantity = Qy - qy
             medicine.save()
@@ -210,102 +217,103 @@ def issueMedicine(request):
             # Signal for medicine issued
         else:
             print('Hello')
-            #signal for wrong quantity input
+            # signal for wrong quantity input
         redirect('issueMedicine')
-    
-    context = {'form' : form}
+
+    context = {'form': form}
     return render(request, 'issueMedicine.html', context)
 
 
-# appointment booking 
+# appointment booking
 @login_required(login_url='/login/')
 def RequestAppointment(request):
     current_user = request.user
-    id=current_user.id-2
+    id = current_user.id-2
 
     try:
-        doctor=request.GET["D_name"]
+        doctor = request.GET["D_name"]
     except MultiValueDictKeyError:
         doctor = False
     try:
-        timings=request.GET["meeting-time"]
+        timings = request.GET["meeting-time"]
     except MultiValueDictKeyError:
         timings = False
     try:
-        Mailid=request.GET["mailid"]
+        Mailid = request.GET["mailid"]
     except MultiValueDictKeyError:
         Mailid = False
-    
-    pid=Patient.objects.get(Pid=id)
-    
-    did=Doctor.objects.get(name=doctor)
+
+    pid = Patient.objects.get(Pid=id)
+
+    did = Doctor.objects.get(name=doctor)
     print(pid)
     print(did)
     print(timings)
-    
-    p= Appointment(Pid=pid,Did=did,Timings=timings,mailid=Mailid)
+
+    p = Appointment(Pid=pid, Did=did, Timings=timings, mailid=Mailid)
     p.save()
-    return render(request,'RequestAppointment.html')
-   
+    return render(request, 'RequestAppointment.html')
+
 # update patient profile
+
+
 def updatepatient(request):
     current_user = request.user
-    
-    pid=current_user.id
+
+    pid = current_user.id
     print(current_user)
     print(pid)
 
     try:
         name = request.GET["name"]
-        
+
     except MultiValueDictKeyError:
         name = False
     try:
         age = request.GET["age"]
-        
+
     except MultiValueDictKeyError:
         age = False
     try:
         gender = request.GET["gender"]
-        
+
     except MultiValueDictKeyError:
         gender = False
-        
-    p=Patient.objects.get(Pid=pid)   
-    p.name=name
-    p.age=age
-    p.gender=gender
+
+    p = Patient.objects.get(Pid=pid)
+    p.name = name
+    p.age = age
+    p.gender = gender
     p.save()
-    return render(request,'UpdatepatientProfile.html')
+    return render(request, 'UpdatepatientProfile.html')
 
 
 # pateint history
 def patientHistory(request):
     print(request.user.id-2)
-    my_history=None
-    pat=None
-    if request.user.patient==True:
-        my_history=PatientHistory.objects.filter(Pid=request.user.id-2)
-        
-        pat=Patient.objects.get(Pid=request.user.id-2)
+    my_history = None
+    pat = None
+    if request.user.patient == True:
+        my_history = PatientHistory.objects.filter(Pid=request.user.id-2)
+
+        pat = Patient.objects.get(Pid=request.user.id-2)
     else:
-        id=request.GET['patid']
-        my_history=PatientHistory.objects.filter(Pid=id)
-        pat=Patient.objects.get(Pid=id)
-   
-    #print(request.user.id)
-    
-    context={'my_his':my_history,'my_pat':pat}
-    return render(request, 'PatientHistory.html',context)
+        id = request.GET['patid']
+        my_history = PatientHistory.objects.filter(Pid=id)
+        pat = Patient.objects.filter(Pid=id).first()
+
+    # print(request.user.id)
+
+    context = {'my_his': my_history, 'my_pat': pat}
+    return render(request, 'PatientHistory.html', context)
 
 
 # schedule test
 def TestSchedule(request):
-    return render(request,'TestSchedule.html')
-
+    return render(request, 'TestSchedule.html')
 
     # current_medicine = request.Medicine
-    
+
     # mid=current_medicine.Mid
     # name=current_medicine.Name
     # type=current_medicine.Type
@@ -314,51 +322,51 @@ def TestSchedule(request):
     # supplier=current_medicine.Supplier
     # purchaseDate=current_medicine.PurchaseDate
     # expiryDate=current_medicine.ExpiryDate
-    
+
     # print(current_medicine)
     # print(mid)
 
     # try:
     #     name = request.GET["Name"]
-        
+
     # except MultiValueDictKeyError:
     #     name = False
     # try:
     #     type = request.GET["Type"]
-        
+
     # except MultiValueDictKeyError:
     #     type = False
     # try:
     #     quantity = request.GET["Quantity"]
-        
+
     # except MultiValueDictKeyError:
     #     quantity = False
-    
+
     # try:
     #     usage = request.GET["Usage"]
-        
+
     # except MultiValueDictKeyError:
     #     usage = False
 
     # try:
     #     supplier = request.GET["Supplier"]
-        
+
     # except MultiValueDictKeyError:
     #     supplier = False
-    
+
     # try:
     #     purchaseDate = request.GET["PurchaseDate"]
-        
+
     # except MultiValueDictKeyError:
     #     purchaseDate = False
-    
+
     # try:
     #     expiryDate = request.GET["ExpiryDate"]
-        
+
     # except MultiValueDictKeyError:
     #     expiryDate = False
-        
-    # p=Medicine.objects.get(Mid=mid)   
+
+    # p=Medicine.objects.get(Mid=mid)
     # p.Name=name
     # p.Type=type
     # p.Quantity=quantity
@@ -368,4 +376,3 @@ def TestSchedule(request):
     # p.ExpiryDate=expiryDate
     # p.save()
     # return render(request,'checkMedicine.html')
-
